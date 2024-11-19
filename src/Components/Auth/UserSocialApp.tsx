0a65b94b43936595de712851b431b0a8bc3a2065
developer <webapp342@gmail.com>
2024-11-19 15:30:15 +0530

    changes in UserSocialApp

--- src/Components/Auth/UserSocialApp.tsx
+++ src/Components/Auth/UserSocialApp.tsx
@@ -1,31 +1,24 @@
-import { FacebookIcon, LinkedInIcon, TwitterIcon } from "@/Constant";
 import { Facebook, Linkedin, Twitter } from "react-feather";
-import { toast } from "react-hot-toast";
-import { Button } from "reactstrap";
+import Link from "next/link";
+import { FacebookIcon, LinkedInIcon, TwitterIcon } from "@/Constant";
 
 const UserSocialApp = () => {
-
-  const handlesubmit = () => {
-    toast.error("This is only for demo purpose, click on the Sign In button to login.");
-  };
-
   return (
-    <div className='social mt-4'>
-      <div className='btn-showcase'>
-        <Button tag='a' color='light' onClick={handlesubmit} target='_blank'>
-          <Linkedin className='txt-linkedin' /> {LinkedInIcon}
-        </Button>
-        <Button tag='a' color='light' onClick={handlesubmit} target='_blank'>
-          <Twitter className='txt-twitter' />
-          {TwitterIcon}
-        </Button>
-        <Button tag='a' color='light' onClick={handlesubmit} target='_blank'>
-          <Facebook className='txt-fb' />
-          {FacebookIcon}
-        </Button>
+    <div className="social mt-4">
+      <div className="btn-showcase">
+        <Link href="https://www.linkedin.com/login" className="btn btn-light" target="_blank">
+          <Linkedin className="txt-linkedin" /> {LinkedInIcon}
+        </Link>
+        <Link href="https://twitter.com/login?lang=en" className="btn btn-light" target="_blank">
+          <Twitter className="txt-twitter" /> {TwitterIcon}
+        </Link>
+        <Link href="https://www.facebook.com/" className="btn btn-light" target="_blank">
+          <Facebook className="txt-fb" /> {FacebookIcon}
+        </Link>
       </div>
     </div>
   );
 };
 
 export default UserSocialApp;
+
