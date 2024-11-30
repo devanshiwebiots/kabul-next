351e416f05874807f8736462bfe77168d807049e
developer <devanshi.webiots@gmail.com>
2024-11-30 12:00:53 +0530

    change

--- src/Components/Applications/Project/ProjectCreate/UploadProjectFile.tsx
+++ src/Components/Applications/Project/ProjectCreate/UploadProjectFile.tsx
@@ -20,13 +20,13 @@ const UploadProjectFile = () => {
         <FormGroup>
           <Label check>{UploadProjectFiles}</Label>
           <Dropzone onChange={updateFiles} value={files} maxFiles={1} header={false} footer={false} minHeight='80px' label="Drag'n drop files here or click to Browse" className="dropzone bg-light-primary ">
-            {files.map((file: ExtFile) => (
+            {files?.map((file: ExtFile) => (
               <FileMosaic key={file.id} {...file} onDelete={removeFile} info={true} />
             ))}
-            {files.length === 0 && (
+            {files?.length === 0 && (
                 <div className='dz-message needsclick'>
                   <i className='icon-cloud-up fs-1 txt-primary'></i>
-                  <h6 className='f-w-700 mb-1'>Drop files here or click to upload.</h6>
+                  <h5 className='f-w-600'>Drop files here or click to upload.</h5>
                   <h6 className='note needsclick'>
                     (This is just a demo dropzone. Selected files are <strong>not</strong> actually uploaded.)
                   </h6>
