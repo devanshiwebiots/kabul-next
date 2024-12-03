b9808e7449283d71eba02aebfb8c580abdea05d8
developer <devanshi.webiots@gmail.com>
2024-12-03 16:06:21 +0530

    change

--- src/Components/Applications/Ecommerce/Product/ProductFeatures/ProductSidebar/SearchBox.tsx
+++ src/Components/Applications/Ecommerce/Product/ProductFeatures/ProductSidebar/SearchBox.tsx
@@ -1,8 +1,8 @@
-import { Col, Form, FormGroup, Input } from "reactstrap";
 import { Search } from "@/Constant";
-import { useState } from "react";
 import { useAppDispatch } from "@/Redux/Hooks";
 import { filterSearchBy } from "@/Redux/Reducer/FilterSlice";
+import { useState } from "react";
+import { Col, Form, Input } from "reactstrap";
 
 const SearchBox = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
@@ -16,10 +16,10 @@ const SearchBox = () => {
   return (
     <Col md={9} sm={12}>
       <Form>
-        <FormGroup className="form-group m-0">
+        <div className="form-group m-0">
           <Input type="search" placeholder={Search} onChange={(e) => handleSearchKeyword(e.target.value)} />
           <i className="fa fa-search"></i>
-        </FormGroup>
+        </div>
       </Form>
     </Col>
   );
