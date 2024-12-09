bd3056781491c16d2ac71281874e622c7d7d37b8
developer <devanshi.webiots@gmail.com>
2024-12-09 09:33:02 +0530

    group-chat done

---
+++ src/app/(Main Body)/chat/group-chat/page.tsx
@@ -0,0 +1,9 @@
+import GroupChatContainer from "@/Components/Applications/Chat/GroupChat"
+
+const PrivateChat = () => {
+    return (
+        <GroupChatContainer />
+    )
+}
+
+export default PrivateChat
