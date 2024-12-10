8ebbffdfea84afd39f0c065ccd9d606a0c2d5d60
developer <devanshi.webiots@gmail.com>
2024-12-10 15:34:09 +0530

    changes

--- src/Components/Forms&Table/Forms/FormControls/InputMask/DefaultInputMaskForm.tsx
+++ src/Components/Forms&Table/Forms/FormControls/InputMask/DefaultInputMaskForm.tsx
@@ -20,60 +20,31 @@ const CleaveInput: React.FC<CleaveInputType> = ({ className, options, placeholde
 
 const DefaultInputMaskForm = () => {
 
-  const currencyOptions = {
-    numeral: true,
-    numeralThousandsGroupStyle: "thousand" as NumeralThousandsGroupStyleType,
-  };
-
-  const prefixOptions = {
-    prefix: "PREFIX",
-    delimiter: "-",
-    blocks: [6, 4, 4, 4],
-    uppercase: true,
-  };
-
-  const tailprefixOptions = {
-    tailPrefix: true,
-    delimiter: ",",
-    blocks: [2,3,3,3],
-    uppercase: true,
-  };
-
-  const delimiterOptions = {
-    delimiters: [".", ".", "-"],
-    blocks: [3, 3, 3],
-    uppercase: true,
-  };
-
-  const cardNumberOptions = {
-    creditCard: true,
-  };
-
   return (
     <Form>
       <Row className="g-3">
         <Col xxl={4} sm={6}>
           <Label className="col-form-label" htmlFor="cleave-number-format">{Currency}</Label>
-          <CleaveInput className="form-control" options={currencyOptions} placeholder={CurrencyPlaceholder} />
+          <CleaveInput className="form-control" options={{ numeral: true, numeralThousandsGroupStyle: "thousand" as NumeralThousandsGroupStyleType }} placeholder={CurrencyPlaceholder} />
         </Col>
         <Col xxl={4} sm={6}>
           <Label className="col-form-label" htmlFor="cleave-type-prefix">{Prefix}</Label>
-          <CleaveInput className="form-control" options={prefixOptions} placeholder={PrefixPlaceholder} />
+          <CleaveInput className="form-control" options={{ prefix: "PREFIX", delimiter: "-", blocks: [6, 4, 4, 4], uppercase: true }} placeholder={PrefixPlaceholder} />
         </Col>
         <Col xxl={4} sm={6}>
           <Label className="col-form-label" htmlFor="cleave-type-delimiter">{Delimiter}</Label>
-          <CleaveInput className="form-control" options={delimiterOptions} placeholder={DelimiterPlaceholder} />
+          <CleaveInput className="form-control" options={{ delimiters: [".", ".", "-"], blocks: [3, 3, 3], uppercase: true }} placeholder={DelimiterPlaceholder} />
         </Col>
         <Col xxl={4} sm={6}>
           <PhoneInput country={"us"} placeholder={PhoneNumberPlaceholder} inputClass="form-control" />
         </Col>
         <Col xxl={4} sm={6}>
           <Label>{CardNumber}</Label>
-          <CleaveInput className="form-control" options={cardNumberOptions} placeholder={CardNumberPlaceholder} />
+          <CleaveInput className="form-control" options={{ creditCard: true }} placeholder={CardNumberPlaceholder} />
         </Col>
         <Col xxl={4} sm={6}>
           <Label>{Tailprefix}</Label>
-          <CleaveInput className="form-control" options={tailprefixOptions} placeholder={TailprefixPlaceholder} />
+          <CleaveInput className="form-control" options={{ prefix: "€", tailPrefix: true, numeral: true, numeralThousandsGroupStyle: "thousand" }} placeholder={TailprefixPlaceholder} />
         </Col>
       </Row>
     </Form>
